05d2d3d3bfb9347858bc643679a8bf9f0af8231b
achideal <1361371152@qq.com>
2026-04-28 20:15:58 +0800

    chore: 移除 mini-pi 包及其相关代码

--- packages/mini-pi/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./mini-pi-agent/index.js";
-export * from "./mini-pi-ai/index.js";
--- packages/mini-pi/src/mini-pi-agent/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./agent.js";
-export * from "./agent-loop.js";
-export * from "./types.js";
-export * from "./validation.js";
